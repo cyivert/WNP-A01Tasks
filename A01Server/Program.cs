ff15d0fce1ba9b69bfd6b350b40591263f8dd4a0
cyivert <phcyiver2@gmail.com>
2026-01-25 12:44:20 -0500

    Read server IP/port from config and init TcpListener/logger
    - Refactored Program.cs to read server IP address and port from the configuration file using ConfigurationManager and constants.
    - Added initialization of TcpListener and LogManager.
    - Included necessary using directives for configuration, networking, and logging utilities.

--- A01Server/Program.cs
+++ A01Server/Program.cs
@@ -8,9 +8,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using A01Server.utils;
 
 namespace A01Server
 {
@@ -18,6 +22,12 @@ namespace A01Server
     {
         static void Main(string[] args)
         {
+            string ipString = ConfigurationManager.AppSettings[Constants.SERVER_IP];            // Read server IP from config file
+            int port = int.Parse(ConfigurationManager.AppSettings[Constants.SERVER_PORT]);      // Read server port from config file
+
+            IPAddress hostAddress = IPAddress.Parse(ipString);                                  // Parse the IP address string to an IPAddress object named hostAddress
+            TcpListener server = new TcpListener(hostAddress, port);                            // Create a TCP listener named server
+            LogManager logger = new LogManager();                                               // Create a log manager instance named logger
         }
     }
 }
